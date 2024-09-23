27f0e6f19eef767d029b3181202719eb75b97979
ASHEENA- <ASHEENA-@ASHEENA>
2024-09-23 09:02:41 +0530

    My Eight Commit

---
+++ Mymavenproject/src/main/java/accessmodifier/Access_Modifier1.java
@@ -0,0 +1,5 @@
+package accessmodifier;
+
+public class Access_Modifier1 {
+
+}
---
+++ Mymavenproject/src/main/java/accessmodifier/Access_Modifier2.java
@@ -0,0 +1,10 @@
+package accessmodifier;
+
+public class Access_Modifier2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
