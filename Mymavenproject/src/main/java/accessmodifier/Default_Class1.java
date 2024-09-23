4f1a922116bd33c0f6f6d7cf31878bca7c457d1d
ASHEENA- <ASHEENA-@ASHEENA>
2024-09-23 09:07:26 +0530

    My fifteenth Commit

---
+++ Mymavenproject/src/main/java/accessmodifier/Default_Class1.java
@@ -0,0 +1,11 @@
+package accessmodifier;
+
+public class Default_Class1 {
+	 int num=20; // private variable
+	 void display()
+	{
+		System.out.print("Hai");
+	}
+
+
+}
---
+++ Mymavenproject/src/main/java/accessmodifier/Default_Class2.java
@@ -0,0 +1,14 @@
+package accessmodifier;
+
+public class Default_Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Default_Class1 obj= new Default_Class1();
+		obj.display();
+	
+		}
+
+	}
+
+
