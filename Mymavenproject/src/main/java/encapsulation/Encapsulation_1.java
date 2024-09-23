4e6820b80fe9e1e1b430bbeec0a39458290c7d5d
ASHEENA- <ASHEENA-@ASHEENA>
2024-09-23 08:51:42 +0530

    My second Commit

---
+++ Mymavenproject/src/main/java/encapsulation/Encapsulation_1.java
@@ -0,0 +1,19 @@
+package encapsulation;
+
+public class Encapsulation_1 {
+	private String Name;
+	private int age;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ Mymavenproject/src/main/java/encapsulation/Encapsulation_2.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class Encapsulation_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation_1 obj=new Encapsulation_1();
+		obj.setName("Asheena");
+		obj.setAge(24);
+		System.out.println(obj.getName());
+		System.out.println(obj.getAge());
+	}
+
+}
