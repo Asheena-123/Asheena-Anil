9c4f8a90fe34b67c5ba918d16a812a4800ab03be
ASHEENA- <ASHEENA-@ASHEENA>
2024-09-23 09:10:50 +0530

    My Inheritance Program

---
+++ Mymavenproject/src/main/java/inheritance/Single_Inheritance1.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Single_Inheritance1 {
+	public void show()
+	{
+		System.out.println("Running");
+	}
+
+}
---
+++ Mymavenproject/src/main/java/inheritance/Single_Inheritance2.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Single_Inheritance2 extends Single_Inheritance1{
+	public void display() {
+		System.out.println("Sleeping");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Single_Inheritance2 obj=new Single_Inheritance2();
+		obj.show();
+	}
+
+}
