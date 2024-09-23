d9052f90245689378eeecf5f1bc0bee5f7850fb1
ASHEENA- <ASHEENA-@ASHEENA>
2024-09-23 09:04:54 +0530

    My Tenth Commit

---
+++ Mymavenproject/src/main/java/debugging/Factorial.java
@@ -0,0 +1,14 @@
+package debugging;
+
+public class Factorial {
+	
+	public static void main(String [] args) {
+		int a=4, fact=1;
+		for (int i=1;i<=a;i++)
+		{
+			fact=fact*i;
+			System.out.println(fact);
+			}
+	}
+
+}
